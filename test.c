27574bb3e892a5c6038e4e7903625f22f18381e1
maomaoyu8 <ykhua@Outlook.com>
2018-11-04 22:20:49 +0800

    Update test.c
    add C source

--- test.c
+++ test.c
@@ -1,1 +1,21 @@
-1111
+#include <stdio.h>
+struct Data
+{
+  int year;
+  int month;
+  int day;
+};
+struct Book
+{
+  int id;
+  char name[20];
+  struct Date date;
+};
+int main(void)
+{
+  struct Book book1;
+  kook1.id = 1;
+  
+  
+  return 0;
+}
